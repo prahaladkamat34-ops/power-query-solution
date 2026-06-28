84e94c9e8c074afd08171cc1c656b6d3b571aabe
prahlad <prahaladkamat34@gmail.com>
2026-06-28 15:06:21 +0530

    add two user record

--- ecommerce.sql
+++ ecommerce.sql
@@ -5,3 +5,9 @@ create table sales_data
     sales_person varchar(50),
     location varchar(10)
 );
+
+insert into sales_data
+(sales_id, sales_person, location )
+values
+(1, 'aayush kamat','noida'),
+(2,'prahlad kamat','delhi');
